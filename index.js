5d29f6b806fe47e6351374bcb9accd4ed8b11fb0
mahlilmahee <mahlilmahee942@gmail.com>
2023-01-01 02:06:06 +0600

    adde

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors()) ;
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ufugb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = 'mongodb+srv://carhub:jfslkaJFDGALK@cluster0.ufugb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 console.log(uri)
 async function run() {
